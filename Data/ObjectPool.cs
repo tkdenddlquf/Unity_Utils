24c242d940849a0717f8ae7740d8cd480eea5f78
tkdenddlquf@naver.com <tkdenddlquf@naver.com>
2024-10-19 18:39:06 +0900

    오브젝트 풀 코드 업데이트

--- Data/ObjectPool.cs
+++ Data/ObjectPool.cs
@@ -3,8 +3,11 @@ using UnityEngine;
 
 public class ObjectPool<T> where T : Object
 {
-    private readonly T poolObject;
+    public System.Action<T> DeAction { private get; set; }
+    public System.Action<T> EnAction { private get; set; }
+
     private readonly Transform parent;
+    private readonly T poolObject;
 
     private readonly Queue<T> poolObjects = new();
 
@@ -18,11 +21,17 @@ public class ObjectPool<T> where T : Object
     {
         if (poolObjects.Count == 0) poolObjects.Enqueue(Object.Instantiate(poolObject, parent));
 
+        DeAction?.Invoke(poolObjects.Peek());
+
         return poolObjects.Dequeue();
     }
 
     public void Enqueue(T _object)
     {
+        if (_object == null) return;
+
+        EnAction?.Invoke(_object);
+
         poolObjects.Enqueue(_object);
     }
 }
