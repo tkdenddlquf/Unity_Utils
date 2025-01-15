8663a67716266f784cf312d24d33ddb9c0a4776d
RFGEM <tkdenddlquf@naver.com>
2025-01-15 19:56:15 +0900

    오브젝트 풀링 클래스가 모노 상속받은 클래스만 풀링하도록 변경, 일부 기능 추가

--- Data/ObjectPool.cs
+++ Data/ObjectPool.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectPool<T> where T : Object
+public class ObjectPool<T> where T : MonoBehaviour
 {
     public System.Action<T> OnDequeue { private get; set; }
     public System.Action<T> OnEnqueue { private get; set; }
@@ -10,13 +10,17 @@ public class ObjectPool<T> where T : Object
 
     private readonly Queue<T> poolObjects = new();
 
+    public int Count => poolObjects.Count;
+
     public ObjectPool(T poolObject) => this.poolObject = poolObject;
 
     public void CreateDefault(Transform parent = null) => Enqueue(Object.Instantiate(poolObject, parent));
 
     public T Dequeue(Transform parent = null)
     {
-        T @object = poolObjects.Count == 0 ? Object.Instantiate(poolObject, parent) : poolObjects.Dequeue();
+        T @object = poolObjects.Count == 0 ? Object.Instantiate(poolObject) : poolObjects.Dequeue();
+
+        @object.transform.SetParent(parent);
 
         OnDequeue?.Invoke(@object);
 
@@ -30,5 +34,15 @@ public class ObjectPool<T> where T : Object
         OnEnqueue?.Invoke(@object);
 
         poolObjects.Enqueue(@object);
+    }
+
+    public void Clear()
+    {
+        while (Count != 0) Remove();
+    }
+
+    public void Remove()
+    {
+        Object.Destroy(poolObjects.Dequeue());
     }
 }
