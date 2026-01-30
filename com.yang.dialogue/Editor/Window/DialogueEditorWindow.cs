87d85a3fa2e6fdd144cccda2ec647326157fe590
RFGEM <tkdenddlquf@naver.com>
2026-01-30 14:47:15 +0900

    다이얼로그 so 검사 추가

--- com.yang.dialogue/Editor/Window/DialogueEditorWindow.cs
+++ com.yang.dialogue/Editor/Window/DialogueEditorWindow.cs
@@ -220,7 +220,7 @@ namespace Yang.Dialogue.Editor
             {
                 SO = so;
 
-                RefreshView();
+                if (so != null) RefreshView();
             }
         }
 
